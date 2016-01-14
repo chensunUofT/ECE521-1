345d2769fc80721c9f3cee743bcfd49c1d261e80
Davi Frossard <davienf1@gmail.com>
2016-01-13 21:08:59 -0500

    Multivariate regression implemented, need to figure out normalization method

--- GradientDescent.py
+++ GradientDescent.py
@@ -2,12 +2,13 @@ from numpy import *
 from pylab import *
 from matplotlib.pyplot import *
 
-def gradientDescentStep(x, y, theta, alpha):
+# Do a single step of gradient descent considering quadratic error
+def gradientDescentStep(x, y, theta, alpha, order):
     y_model = x.dot(theta).flatten()
     error = (y.flatten() - y_model)
     m = y.size
-    theta[0] += alpha * (1.0/m) * (error).sum();
-    theta[1] += alpha * (1.0/m) * (error*x[:,1]).sum();
+    for i in range(0, order):
+        theta[i] += alpha * (1.0/m) * (error*x[:,i]).sum();
     return theta
 
 # Generate artificial dataset
@@ -16,18 +17,22 @@ train_y = sin(train_x) + 0.1 * power(train_x, 2) + 0.5 * random.randn(100, 1)
 tSize = train_x.size
 
 # Precalculate input powers
-xp = zeros(shape=(tSize,2))
-for i in range(2):
+order = int(raw_input("Enter polynomial order: "))+1
+xp = zeros(shape=(tSize,order))
+for i in range(order):
     xp[:,i] = power(train_x, i)[:,0]
+xpn = xp / xp.max(axis=0)
 
 # Linear regression parameters
-iterations = 2000
-lRate = 0.001
-theta = [0,0]
+iterations = 200000
+lRate = 0.000001
+theta = zeros(order)
 
+# Perform Gradient Descent
 for i in range(iterations):
-    theta = gradientDescentStep(xp, train_y, theta, lRate)
+    theta = gradientDescentStep(xp, train_y, theta, lRate, order)
 
+# Display Results
 print(theta)
 scatter(train_x, train_y, marker='x', color='r', label='Training Data')
 result = xp.dot(theta).flatten()
