19392f11536be09d5cca8ea8b46afe5502a6012c
Davi Frossard <davienf1@gmail.com>
2016-03-20 02:30:27 -0400

    Better function documentation

--- Assignment 3/distance_functions.py
+++ Assignment 3/distance_functions.py
@@ -12,9 +12,9 @@ def square_distance(x, y):
     '''
     Returns pairwise squared distance between two matrices:
     For points xi belonging to X and yj belonging to Y, we have:
-        d(xi,yj)^2 = ||xi - yj||^2 = ||xi||^2 + ||xj||^2 - 2 * (yj' . xi)
+        d(xi,yj)^2 = ||xi - yj||^2 = ||xi||^2 + ||xj||^2 - 2 * (yj . xi)
     Expanding to the entire matrix, we get:
-        d(X, Y)^2 = (||X||^2' .+ ||Y||^2) - 2*dot(Y',X)
+        d(X, Y)^2 = (||X||^2 .+ ||Y||^2) - 2*dot(Y,X')
             Where .+ is the outer (element-wise) sum
     '''
 
